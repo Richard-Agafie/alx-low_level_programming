85f1f51793b5e2a8088791e302523c2bd50d6ff7
Richard-Agafie <favrichie1@gmail.com>
2023-03-22 04:33:17 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@ void print_times_table(int n)
 	{
 	int i, j, k;
 
-
 	if (n >= 0 && n <= 15)
 	{
 	for (i = 0; i <= n; i++)
@@ -45,6 +44,4 @@ void print_times_table(int n)
 	}
 	}
 	_putchar('\n');
-	}
-	}
-	}
+	}	}	}
